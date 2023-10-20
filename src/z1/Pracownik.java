e934c040e1450957a9e9a439d8a37aa0232f2c57
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-20 09:00:27 +0200

    z1

---
+++ src/z1/Pracownik.java
@@ -0,0 +1,24 @@
+package z1;
+
+public final class Pracownik {
+    private int pensja;
+    private Pracownik przelozony;
+
+    public Pracownik(Pracownik przelozony) {
+        this.przelozony = przelozony;
+        this.pensja = 3600;
+    }
+
+    public Pracownik(Pracownik przelozony, int pensja) {
+        this.przelozony = przelozony;
+        this.pensja = pensja;
+    }
+
+    public int getPensla() {
+        return pensja;
+    }
+
+    public Pracownik getPrzelozony() {
+        return przelozony;
+    }
+}
